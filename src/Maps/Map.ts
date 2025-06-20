15986a99fd2693cca7fc96f018a2c9a0b7242e40
Curtis Slone <curtisdslone@gmail.com>
2025-06-19 18:35:32 -0600

    Map Update. Included config

--- src/Maps/Map.ts
+++ src/Maps/Map.ts
@@ -1,5 +1,15 @@
 import * as ex from 'excalibur';
+import { GameConfig } from '../GameConfig.js';
+import { MapConfig } from './MapConfig.js';
 
 export class Map extends ex.Actor {
-    
+   constructor() {
+        super({
+             pos: ex.vec(0, 0),
+             width: GameConfig.width,
+             height: GameConfig.height,
+             scale: MapConfig.mapScale,
+             anchor: MapConfig.mapAnchor
+        });
+     }
 }
---
+++ src/Maps/MapConfig.ts
@@ -0,0 +1,6 @@
+import * as ex from 'excalibur';
+
+export const MapConfig = {
+    mapScale: ex.vec(2, 2),
+    mapAnchor: ex.vec(0, 0)
+} as const;
